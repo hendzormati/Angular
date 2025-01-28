103fc195de03394c8c30f520e881946baf680917
hendesprit <hend.zormati@esprit.tn>
2025-01-28 12:14:43 +0100

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+name="my home"; //interpolation
+status=true;
+i =0;
+// event biding
+add(){this.i++;console.log("salut"); this.name=" clicked "+this.i;}
+
+}
