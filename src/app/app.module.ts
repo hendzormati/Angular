0c0bd613e2496e248b2cc55191a2e6c11d5985af
hendesprit <hend.zormati@esprit.tn>
2025-02-04 15:28:18 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,30 @@ import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { ResidenceComponent } from './residence/residence.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+// routing
+const routes:Routes=[
+  {path:"",redirectTo:'home',pathMatch:"full"},
+  {path:"home",component:HomeComponent},
+  {path:"residence",component:ResidenceComponent},
+  // always the last one 
+  {path:"**",component:NotfoundComponent},
+
+]
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
     FooterComponent,
-    ResidenceComponent
+    ResidenceComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
